f7f79ba3021bd9484fb9e078c6fb4d9ca835c431
ist1113318 <pedro.ferreira.da.costa@tecnico.ulisboa.pt>
2025-03-09 18:48:23 +0000

    update

--- ServerApp.java
+++ ServerApp.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.net.*;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -21,40 +20,26 @@ public class ServerApp {
             KeyPair keyPairServerB = keyGen.generateKeyPair();
             KeyPair keyPairServerC = keyGen.generateKeyPair();
             KeyPair keyPairServerD = keyGen.generateKeyPair();
-            KeyPair keyPairLeader = keyGen.generateKeyPair();
 
             // Create crypto instances for communication
             cryptoClass cryptoClient_ServerA = new cryptoClass(keyPairClient.getPrivate(), keyPairServerA.getPublic());
             cryptoClass cryptoClient_ServerB = new cryptoClass(keyPairClient.getPrivate(), keyPairServerB.getPublic());
             cryptoClass cryptoClient_ServerC = new cryptoClass(keyPairClient.getPrivate(), keyPairServerC.getPublic());
             cryptoClass cryptoClient_ServerD = new cryptoClass(keyPairClient.getPrivate(), keyPairServerD.getPublic());
-            cryptoClass cryptoClient_Leader = new cryptoClass(keyPairClient.getPrivate(), keyPairLeader.getPublic());
 
-            cryptoClass cryptoServerA_Leader = new cryptoClass(keyPairServerA.getPrivate(), keyPairLeader.getPublic());
-            cryptoClass cryptoServerB_Leader = new cryptoClass(keyPairServerB.getPrivate(), keyPairLeader.getPublic());
-            cryptoClass cryptoServerC_Leader = new cryptoClass(keyPairServerC.getPrivate(), keyPairLeader.getPublic());
-            cryptoClass cryptoServerD_Leader = new cryptoClass(keyPairServerD.getPrivate(), keyPairLeader.getPublic());
-
-            cryptoClass cryptoLeader_ServerA = new cryptoClass(keyPairLeader.getPrivate(), keyPairServerA.getPublic());
-            cryptoClass cryptoLeader_ServerB = new cryptoClass(keyPairLeader.getPrivate(), keyPairServerB.getPublic());
-            cryptoClass cryptoLeader_ServerC = new cryptoClass(keyPairLeader.getPrivate(), keyPairServerC.getPublic());
-            cryptoClass cryptoLeader_ServerD = new cryptoClass(keyPairLeader.getPrivate(), keyPairServerD.getPublic());
-
-            // Store crypto instances in lists
-            List<cryptoClass> cryptoServerList = Arrays.asList(cryptoServerA_Leader, cryptoServerB_Leader, cryptoServerC_Leader, cryptoServerD_Leader);
-            List<cryptoClass> cryptoClientList = Arrays.asList(cryptoClient_ServerA, cryptoClient_ServerB, cryptoClient_ServerC, cryptoClient_ServerD, cryptoClient_Leader);
-            List<cryptoClass> cryptoLeaderList = Arrays.asList(cryptoLeader_ServerA, cryptoLeader_ServerB, cryptoLeader_ServerC, cryptoLeader_ServerD);
+            cryptoClass cryptoServerA_Client = new cryptoClass(keyPairServerA.getPrivate(), keyPairClient.getPublic());
+            cryptoClass cryptoServerB_Client = new cryptoClass(keyPairServerB.getPrivate(), keyPairClient.getPublic());
+            cryptoClass cryptoServerC_Client = new cryptoClass(keyPairServerC.getPrivate(), keyPairClient.getPublic());
+            cryptoClass cryptoServerD_Client = new cryptoClass(keyPairServerD.getPrivate(), keyPairClient.getPublic());
 
             // Start the Client
-            new Thread(new ClientBFT("Client", new networkClass(4000), cryptoClientList)).start();
+            new Thread(new ClientBFT("Client", new networkClass(4000), Arrays.asList(cryptoClient_ServerA, cryptoClient_ServerB, cryptoClient_ServerC, cryptoClient_ServerD))).start();
 
             // Start the 4 Normal Servers on different ports
-            for (int i = 0; i < 4; i++) {
-                new Thread(new ServerBFT(name_list.get(i), new networkClass(5000 + i), cryptoServerList.get(i), 5000 + i)).start();
-            }
-
-            // Create Leader Server
-            new Thread(new leaderBFT("Leader", new networkClass(6000), cryptoLeaderList)).start();
+            new Thread(new ServerBFT("A", new networkClass(5000), cryptoServerA_Client, 5000)).start();
+            new Thread(new ServerBFT("B", new networkClass(5001), cryptoServerB_Client, 5001)).start();
+            new Thread(new ServerBFT("C", new networkClass(5002), cryptoServerC_Client, 5002)).start();
+            new Thread(new ServerBFT("D", new networkClass(5003), cryptoServerD_Client, 5003)).start();
 
             System.out.println("All servers are running...");
         } catch (NoSuchAlgorithmException | SocketException e) {
